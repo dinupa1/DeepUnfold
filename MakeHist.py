82e360065e3a4e3485c81dad9d625828946482a6
dinupa1 <dinupa3@gmail.com>
2023-02-05 00:09:39 -0600

    bug fix

--- MakeHist.py
+++ MakeHist.py
@@ -9,6 +9,7 @@ import uproot
 import hist
 from hist import Hist
 import numba
+import awkward as ak
 from numba import njit
 
 pi = np.pi
@@ -26,15 +27,23 @@ def weight2D(lam, mu, nu, phi, costh):
 # @njit()
 def sample_mc(nsamples, data, out_file):
 
-    hist2D = Hist(hist.axis.Regular(20, -pi, pi, name="phi"),
-                  hist.axis.Regular(20, -1., 1., name="costh"))
+    # lambda_array = np.random.uniform(-1., 1., nsamples)
+    # mu_array = np.random.uniform(-1., 1., nsamples)
+    # nu_array = np.random.uniform(-1., 1., nsamples)
 
-    lambda_array = np.random.normal(0.5, 0.01, nsamples)
-    mu_array = np.random.normal(0.0, 0.01, nsamples)
-    nu_array = np.random.normal(0.0, 0.01, nsamples)
+    lambda_array = np.random.normal(0.2, 0.01, nsamples)
+    mu_array = np.random.normal(0.1, 0.01, nsamples)
+    nu_array = np.random.normal(-0.1, 0.01, nsamples)
+
+    hist_array = []
 
     for i in range(nsamples):
+
+        hist2D = Hist(hist.axis.Regular(20, -pi, pi, name="phi"),
+                  hist.axis.Regular(20, -1., 1., name="costh"))
+
         print("sample = {}, lambda = {:.3f}, mu = {:.3f}, nu = {:.3f}".format(i, lambda_array[i], mu_array[i], nu_array[i]))
+
         hist2D.fill(data["phi"], data["costh"], weight=data["weight"]* weight2D(lambda_array[i], mu_array[i], nu_array[i], data["true_phi"], data["true_costh"]))
 
         hist_name = "hist_{}_{}_{}".format(lambda_array[i], mu_array[i], nu_array[i])
@@ -44,17 +53,14 @@ def sample_mc(nsamples, data, out_file):
     print("**** monte-carlo sampling done ****")
 
 
-nsamples = 100
+nsamples = 50
 
-tree = uproot.open("DY_DUMP_4pi_GMC_Jan08_LD2.root:result_mc")
+tree = uproot.open("data/DY_DUMP_4pi_GMC_Jan08_LD2.root:result_mc")
 
 data = tree.arrays(["weight", "phi", "costh", "true_phi", "true_costh"],
                        "(fpga1==1) & (4.5 < true_mass) & (true_mass < 8.0) & (mass > 0.)", library="np")
 
-print("****     *****")
-print(data["weight"].shape)
-print("****     ****")
 
-out_file = uproot.recreate("dummy.root", compression=uproot.ZLIB(4))
+out_file = uproot.recreate("data/dummy.root", compression=uproot.ZLIB(4))
 
 sample_mc(nsamples, data, out_file)
